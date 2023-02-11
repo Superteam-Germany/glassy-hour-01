ee3701a0b1ec33dfbc4c8ff4883bce3dc053432f
Yusuf-Uluc <yusuf.uluc@protonmail.com>
2023-02-11 14:19:18 +0100

    outdir

--- app/blog-ui/astro.config.mjs
+++ app/blog-ui/astro.config.mjs
@@ -6,6 +6,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: 'server',
+  outDir: 'dist',
   integrations: [svelte(), tailwind(), prefetch()],
   adapter: vercel(),
 });
